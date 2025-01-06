a39b423bd780122ce7132ab2bb8766e2e43103ae
이기종 <rlwhd0716@gmail.com>
2025-01-06 09:22:34 +0900

    [Domain Module] UseCase 1. Create Base Use Case 2. Get Posts Use Case

--- domain/lib/usecase/post_list_usecase.dart
+++ domain/lib/usecase/post_list_usecase.dart
@@ -1,4 +1,3 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:domain/domain.dart';
 import 'package:domain/usecase/base_usecase.dart';
@@ -8,7 +7,7 @@ import 'package:injectable/injectable.dart';
 class PostListUseCase implements BaseUseCase<void, List<PostModel>> {
   final PostsRepository _postsRepository;
 
-  PostListUseCase(this._postsRepository)
+  PostListUseCase(this._postsRepository);
 
   @override
   Future<Either<String, List<PostModel>>> execute(void input) async {
