3efedeb57401c36f5e211bf1156d42070d922f2a
이기종 <rlwhd0716@gmail.com>
2025-01-09 18:01:09 +0900

    [Comment] Add Comment

--- lib/routes_manager.dart
+++ lib/routes_manager.dart
@@ -5,6 +5,12 @@ import 'package:flutter_clean_architecture/posts/bloc/posts_bloc.dart';
 import 'package:flutter_clean_architecture/posts/view/posts_view.dart';
 import 'package:go_router/go_router.dart';
 
+/**
+ * 화면 전환 Manager
+ * Routes 클래스에 String으로 경로 설정
+ * Generator에 화면과 Bloc을 할당하여 이동될 수 있도록 초기화
+ **/
+
 class Routes {
   static const String splashRoute = "/";
   static const String postsRoute = "/posts";
