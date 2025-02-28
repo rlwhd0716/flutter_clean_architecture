63503423555f495de1229d6f01dcc65cfe7abb14
LeeGiJong <rlwhd0716@gmail.com>
2025-02-28 17:16:40 +0900

    [Build Script] 1. 자주 사용하거나, 한 번 실행할 때 모든 패키지에서 실행되어야 하는 스크립트 러너 2. Build Runner 세가지 옵션 제공 3. flutter 패키지 clean, get 4. 원하는 기능을 추가할 수 있도록 수정

--- lib/execute.dart
+++ lib/execute.dart
@@ -5,31 +5,42 @@ void main() {
   final List<String> projectPaths = [
     '$currentDir/packages/data',
     '$currentDir/packages/domain',
+    '$currentDir/packages/util',
     currentDir,
   ];
 
-  print("Build Runner 실행 프로그램 winitech_md_sb v1");
-  print("1. 기본 build 실행");
-  print("2. --delete-conflicting-outputs 추가");
-  print("3. clean 실행");
-  print("4. watch 모드 실행");
+  print("Dart Run MD v1");
+  print("1. dart run build 실행");
+  print("2. build --delete-conflicting-outputs 실행");
+  print("3. watch 모드 실행");
+  print("4. flutter pub get 실행");
+  print("5. clean 실행");
 
   stdout.write("원하는 작업 번호를 입력하세요: ");
   final commandChoice = stdin.readLineSync();
 
+  String executable;
   List<String> args;
   switch (commandChoice) {
     case '1':
-      args = ['build'];
+      executable = 'dart';
+      args = ['run', 'build_runner', 'build'];
       break;
     case '2':
-      args = ['build', '--delete-conflicting-outputs'];
+      executable = 'dart';
+      args = ['run', 'build_runner', 'build', '--delete-conflicting-outputs'];
       break;
     case '3':
-      args = ['clean'];
+      executable = 'dart';
+      args = ['run', 'build_runner', 'watch'];
       break;
     case '4':
-      args = ['watch'];
+      executable = 'flutter';
+      args = ['pub', 'get'];
+      break;
+    case '5':
+      executable = 'flutter';
+      args = ['clean'];
       break;
     default:
       print("유효하지 않은 선택입니다.");
@@ -39,17 +50,17 @@ void main() {
   for (final projectPath in projectPaths) {
     if (Directory(projectPath).existsSync()) {
       print("\n[$projectPath]에서 명령어 실행 중...");
-      _runCommand(projectPath, args);
+      _runCommand(projectPath, executable, args);
     } else {
       print("\n[$projectPath] 경로가 존재하지 않습니다. 스킵합니다.");
     }
   }
 }
 
-void _runCommand(String path, List<String> args) async {
+void _runCommand(String path, String executable, List<String> args) async {
   final process = await Process.start(
-    'dart',
-    ['run', 'build_runner', ...args],
+    executable,
+    args,
     workingDirectory: path,
     runInShell: true,
   );
@@ -65,10 +76,10 @@ void _runCommand(String path, List<String> args) async {
   final exitCode = await process.exitCode;
 
   if (exitCode == 0) {
-    print("[$path] 명령어 실행 완료: dart run build_runner ${args.join(' ')}");
+    print("[$path] 명령어 실행 완료: $executable ${args.join(' ')}");
   } else {
     print("[$path] 명령어 실행 중 오류 발생: $exitCode");
   }
 }
 
-// dart lib/build_runner_execute.dart
+// dart lib/execute.dart
